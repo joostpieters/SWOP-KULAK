09ece9135a242b28c2b45cc4917717fc28b59da5
Frederic <Frederic@192.168.0.156>
2015-02-28 23:38:57 +0100

    Best zou je nog geen methodes gebruiken uit task, het kan best zijn dat er methodes wegvallen maar tis al te laat om het vandaag nog af te werken.
    Task constructor added comments and validity check of accDev argument.
    Created:
    Task.getDescription()
    Task.isValidAcceptableDeviation(float accDev) 
    Task.getEstimatedDuration()
    Task.getTimeSpan()
    Task.getAcceptableDeviation()
    Task.getAlternativeTask()
    Task.getPrerequisiteTasks()
    Task.getStatus()
    Task.isAvailable()
    Task.isFulfilled()
    Task.setStatus(Status status)
    Task.isValidTimeSpan(Timespan timeSpan)
    Task.isValidAlternativeTask(Task altTask)
    Task.dependsOn(Task task)
    Task.setAlternativeTask(Task task)
    Task.SetTaskFailed(Timespan timeSpan)
    Task.setTimeSpan(Timespan timeSpan)

--- src/domain/Task.java
+++ src/domain/Task.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
@@ -8,57 +9,323 @@ import java.util.GregorianCalendar;
  * @author Frederic, Mathias, Pieter-Jan
  */
 public class Task {
-    
-    private int id;
-    
-    private String description;
-    
-    private float deviation;
-    
-    private Timespan duration;
-    
-    private Duration estimatedDuration;
-    
-    private Task alternativeTask;
-    
-    private ArrayList<Task> prerequisiteTasks;
-    
-    private Status status;
-    
-    private static int nextId=0;
-    
-    public int getId()
-    {
-    	return this.id;
-    }
-    
-    public Task(String description, int estDur, float accDev, ArrayList<Task> prereq, Status status){
-        this.id = generateId();
-    	//this.estimatedDuration = new Duration(estDur);
-    	this.id = Task.generateId();
-    	this.description = description;
-    	this.deviation = accDev;
-    	this.alternativeTask = alternativeTask;
-    	this.prerequisiteTasks = prereq;
-    	this.status = status;
-    }
-    
-    public void update(GregorianCalendar start, GregorianCalendar end, Status status){
-        
-    }
-    
-    public boolean isAvailable(){
-        return true;
-    }
-    
-    /**
-     * Generates an id for a new task.
-     * 
-     * @return The id to be used for a newly created task.
-     */
-    private static int generateId()
-    {
-    	return nextId++;
-    }
-    
+
+	private int id;
+
+	private String description;
+
+	private float acceptableDeviation;
+
+	private Timespan timeSpan;
+
+	private Duration estimatedDuration;
+
+	private Task alternativeTask;
+
+	private ArrayList<Task> prerequisiteTasks;
+
+	private Status status;
+
+	private static int nextId=0;
+
+	/**
+	 * Initializes this task based on the given description, estimated duration, acceptable deviation,
+	 * prerequisite tasks and status.
+	 * 
+	 * @param description The description of this task.
+	 * @param estDur The estimated duration in minutes of this task.
+	 * @param accDev The acceptable deviation of this task expressed as a float between 0 and 1.
+	 * @param prereq The list of prerequisite tasks which need to be finished before this task can be started.
+	 * @param status The status of this task.
+	 * @throws IllegalArgumentException The given acceptable deviation is not valid.
+	 */
+	public Task(String description, long estDur, float accDev, ArrayList<Task> prereq, Status status){
+		if(!isValidAcceptableDeviation(accDev))
+			throw new IllegalArgumentException("The acceptable deviation has to be between 0 and 1.");
+
+		this.id = generateId();
+		this.estimatedDuration = new Duration(estDur);
+		this.id = Task.generateId();
+		this.description = description;
+		this.acceptableDeviation = accDev;
+		this.prerequisiteTasks = prereq;
+		this.status = status;
+		if(this.prerequisiteTasks == null) // TODO mag dit null zijn?
+			this.prerequisiteTasks = new ArrayList<Task>();
+	}
+
+	/**
+	 * @return The identification number of this task.
+	 */
+	public int getId()
+	{
+		return this.id;
+	}
+
+	/**
+	 * @return The description of this task.
+	 */
+	public String getDescription()
+	{
+		return this.description;
+	}
+
+	/**
+	 * @return The estimated duration of this task.
+	 */
+	public Duration getEstimatedDuration()
+	{
+		return this.estimatedDuration;
+	}
+
+	/**
+	 * @return The timeSpan indicating the actual start and end time of this task.
+	 */
+	public Timespan getTimeSpan()
+	{
+		return this.timeSpan;
+	}
+	/**
+	 * @return The acceptable deviation of this task expressed as a float between 0 and 1.
+	 */
+	public float getAcceptableDeviation()
+	{
+		return this.acceptableDeviation;
+	}
+
+	/**
+	 * @return The alternative task for this task.
+	 */
+	public Task getAlternativeTask()
+	{
+		return this.alternativeTask;
+	}
+
+	/**
+	 * @return The list of prerequisite tasks for this task.
+	 */
+	public ArrayList<Task> getPrerequisiteTasks()
+	{
+		return (ArrayList<Task>)this.prerequisiteTasks.clone();
+	}
+
+	/**
+	 * @return The status of this task.
+	 */
+	public Status getStatus()
+	{
+		return this.status;
+	}
+	/**
+	 * Checks whether the given acceptable deviation is valid.
+	 * 
+	 * @param  accDev The acceptable deviation.
+	 * @return True if and only if the given acceptable deviation is between 0 and 1.
+	 */
+	private boolean isValidAcceptableDeviation(float accDev)
+	{
+		return (0 <= accDev) && (accDev <= 1);
+	}
+	/** TODO unfinished
+	 * Updates the status and the time span of this task.
+	 * 
+	 * @param start The beginning of the time span of this task.
+	 * @param end The end of the time span of this task.
+	 * @param status The new status of this task.
+	 */
+	public void update(LocalDateTime start, LocalDateTime end, Status status){
+		if(status != Status.FINISHED && status != Status.FAILED)
+			throw new IllegalArgumentException("The given status is neither FAILED nor FINISHED and is therefore not a valid status.");
+		Timespan timespan = new Timespan(start, end);
+		if(status.equals(Status.FINISHED))
+			setTaskFinished(timespan);
+		else if (status.equals(Status.FAILED))
+			setTaskFailed(timespan);
+	}
+
+	/**
+	 * Checks whether this task is available.
+	 * 
+	 * @return True if and only if every prerequisite task of this task has been fulfilled.
+	 */
+	public boolean isAvailable(){
+		for(Task prereqTask : getPrerequisiteTasks())
+		{
+			if(!prereqTask.isFulfilled())
+				return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Checks whether this task has been fulfilled.
+	 * 
+	 * @return True if and only if this task is finished or if it has an alternative task
+	 *         and this alternative task is fulfilled.
+	 */
+	public boolean isFulfilled()
+	{
+		if(getStatus().equals(Status.FINISHED)) // TODO replace with isFinished()?
+			return true;
+		if(getAlternativeTask() != null)
+			return getAlternativeTask().isFulfilled();
+		return false;
+	}
+
+	/**
+	 * Sets the status of this task to the given status.
+	 * @param status The new status of this task.
+	 */
+	private void setStatus(Status status)
+	{
+		this.status = status;
+	}
+	
+	/**
+	 * Sets the status of this task to finished and the time span to the given time span if this task is available.
+	 * 
+	 * @param timeSpan The time span of this task.
+	 */
+	public void setTaskFinished(Timespan timeSpan)
+	{
+		if(isAvailable())
+		{
+			setTimeSpan(timeSpan);
+			setStatus(Status.FINISHED);
+		}
+	}
+	
+	/**
+	 * Sets the time span of this task to the given time span.
+	 * 
+	 * @param timeSpan The new time span of this task.
+	 * @throws IllegalArgumentException If the given time span is not valid for this task.
+	 */
+	private void setTimeSpan(Timespan timeSpan)
+	{
+		if(!isValidTimeSpan(timeSpan))
+			throw new IllegalArgumentException("The given time span is not a valid time span for this task");
+		this.timeSpan = timeSpan;
+	}
+	/**
+	 * Sets the status of this task to failed and the time span to the given time span.
+	 */
+	public void setTaskFailed(Timespan timeSpan)
+	{
+		setTimeSpan(timeSpan);
+		setStatus(Status.FAILED);
+	}
+	
+	/**
+	 * Sets this task status to failed, the alternative task to the given alternative task
+	 * and the time span to the given time span.
+	 * 
+	 * @param alternativeTask The alternative task for this task.
+	 * @param timeSpan The time span for this task.
+	 */
+	public void setTaskFailed(Task alternativeTask, Timespan timeSpan)
+	{
+		setTimeSpan(timeSpan);
+		if(!isValidAlternativeTask(alternativeTask))
+			throw new IllegalArgumentException("The given alternative task is not a valid alternative task.");
+		setStatus(Status.FAILED);
+		setAlternativeTask(alternativeTask);
+	}
+	/**
+	 * Checks whether the given time span overlaps with the time span of this task.
+	 * 
+	 * @param timeSpan The time span to check.
+	 * @return True if and only if this task has a time span and the given time span overlaps with it.
+	 */
+	public boolean overlapsWith(Timespan timeSpan)
+	{
+		if(getTimeSpan() == null)
+			return false;
+		return getTimeSpan().overlapsWith(timeSpan);
+	}
+	
+	/**
+	 * Checks whether the given time span is a valid time span for this task.
+	 * 
+	 * @param timeSpan
+	 * @return
+	 */
+	public boolean isValidTimeSpan(Timespan timeSpan) // TODO nog niet af.
+	{
+		if(getPrerequisiteTasks() != null)
+		{
+			for(Task t: getPrerequisiteTasks())
+				if(t.overlapsWith(timeSpan))
+					return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Checks whether the given task is a valid alternative task for this task.
+	 * 
+	 * @param altTask The alternative task to check.
+	 * @return False if the given alternative task is equal to this task.
+	 *         False if this task depends on the given alternative task.
+	 *         False if the given alternative task depends on this task.
+	 *         True otherwise.
+	 */
+	public boolean isValidAlternativeTask(Task altTask)
+	{
+		if(altTask.equals(this))
+			return false;
+		if(this.dependsOn(altTask))
+			return false;
+		if(altTask.dependsOn(this))
+			return false;
+		return true;
+	}
+
+	/**
+	 * Checks whether this task directly or indirectly depends on the given task.
+	 * 
+	 * @param task The task to check.
+	 * @return True if this task has an alternative task and the alternative task is equal to the given task.
+	 *         True if a prerequisite task of this task is equal to the given task.
+	 *         True if a prerequisite task depends on the given task.
+	 *         False otherwise.
+	 */
+	public boolean dependsOn(Task task)
+	{
+		if(getAlternativeTask() != null && getAlternativeTask().equals(task))
+			return true;
+		if(getPrerequisiteTasks() != null)
+		{
+			for(Task prereqTask : getPrerequisiteTasks())
+			{
+				if(prereqTask.equals(task))
+					return true;
+
+				if(prereqTask.dependsOn(task))
+					return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Sets the alternative task of this task to the given alternative task.
+	 * 
+	 * @param alternativeTask The new alternative task for this task.
+	 */
+	private void setAlternativeTask(Task alternativeTask)
+	{
+		this.alternativeTask = alternativeTask;
+	}
+	/**
+	 * Generates an id for a new task.
+	 * 
+	 * @return The id to be used for a newly created task.
+	 */
+	private static int generateId()
+	{
+		return nextId++;
+	}
+
 }
