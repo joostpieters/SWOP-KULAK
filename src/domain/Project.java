585cc4078c22776d0126ed1c0d5b6146a982ad8a
mrTsjolder <mrTsjolder@users.noreply.github.com>
2015-02-28 18:09:43 +0100

    Eerste implementatie. Ik weet dat ik niet getest heb, maar daar zijn redenen voor. (in de rapte geprogrammeerd). Commentaar komt ook nog, maar ik hoop dat je daarmee al iets geholpen bent.
    PS: Ik overweeg enkele parameters uit createTask te halen. (vb: status
    kan afgeleid worden).

--- src/domain/Project.java
+++ src/domain/Project.java
@@ -1,7 +1,11 @@
 package domain;
 
-import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 
 /**
  * This class represents a project
@@ -10,15 +14,166 @@ import java.util.GregorianCalendar;
  */
 public class Project {
     
-    private ArrayList<Task> tasks;
+    private final int id;
+    private final String name;
+    private final String description;
+    private final GregorianCalendar creationTime;
+    private final GregorianCalendar dueTime;
+    private final SortedMap<Integer, Task> tasks = new TreeMap<>();
     
-    public ArrayList<Task> getTasks(){
-        return (ArrayList<Task>) tasks.clone();
-    }
-    
+    /**
+     * 
+     * @param 	id
+     * @param 	name
+     * @param 	descr
+     * @param 	creation
+     * @param 	due
+     */
     public Project(int id, String name, String descr, GregorianCalendar creation, GregorianCalendar due){
-        
+    	if(!this.checkTimeOrder(creation, due))
+    		throw new IllegalArgumentException("The creation time was after the due time.");
+    	
+    	this.id = id;
+    	this.name = name;
+    	this.description = descr;
+    	this.creationTime = creation;
+    	this.dueTime = due;
     }
     
+    /****************************************
+     * Getters & setters					*
+     ****************************************/
     
+    /**
+	 * @return 	the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @return 	the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return 	the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @return 	the creationTime
+	 */
+	public GregorianCalendar getCreationTime() {
+		return creationTime;
+	}
+
+	/**
+	 * @return 	the dueTime
+	 */
+	public GregorianCalendar getDueTime() {
+		return dueTime;
+	}
+
+	/**
+	 * 
+	 * @return	the tasks
+	 */
+	public SortedMap<Integer, Task> getTasks(){
+        return new TreeMap<>(tasks);
+    }
+	
+	/**
+	 * 
+	 * @param 	tid
+	 * @return	
+	 */
+	public Task getTask(int tid) {
+		Task t = tasks.get(id);
+		if(t == null)
+			throw new IllegalArgumentException("Task with tid " + tid + " doesn't exist.");
+		return t;
+	}
+	
+	/**
+	 * 
+	 * @param 	t
+	 */
+	public void addTask(Task t) {
+		if(t == null)
+			throw new NullPointerException("You can't add null-tasks to a project.");
+		this.tasks.put(t.getId(), t);
+	}
+
+    /****************************************
+     * Validation							*
+     ****************************************/
+	
+	/**
+	 * 
+	 * @param start
+	 * @param end
+	 * @return
+	 */
+	//TODO: Information expert?
+	public boolean checkTimeOrder(GregorianCalendar start, GregorianCalendar end) {
+		return start.before(end);
+	}
+    
+    /****************************************
+     * Task-management						*
+     ****************************************/
+    
+	/**
+	 * 
+	 * @param 	descr
+	 * @param 	estDur
+	 * @param 	accdev
+	 * @param 	altFor
+	 * @param 	prereq
+	 * @param 	status
+	 */
+	public void createTask(String descr, int estDur, int accdev, int altFor, int[] prereq, Status status) {
+		Task[] arr = new Task[prereq.length];
+		for(int i : prereq) {
+			arr[i] = getTask(prereq[i]);
+		}
+		//TODO: Constructor for Task!!!!
+		//Task t = new Task(descr, estDur, accdev, getTask(altFor), arr, status);
+		//addTask(t);
+		throw new IllegalStateException("waiting for new task constructor.");
+	}
+	
+	/**
+	 * 
+	 * @param 	tid
+	 * @param 	start
+	 * @param 	end
+	 * @param 	status
+	 */
+	public void updateTask(int tid, GregorianCalendar start, GregorianCalendar end, Status status) {
+		if(!checkTimeOrder(start, end))
+    		throw new IllegalArgumentException("The creation time was after the due time.");
+		getTask(tid).update(start, end, status);
+	}
+	
+	/**
+	 * 
+	 * @return	
+	 */
+	public List<Task> getAvailableTasks() {
+		List<Task> result = new LinkedList<Task>();
+		for(Task t : tasks.values()) {
+			if(t.isAvailable())
+				result.add(t);
+		}
+		return result;
+	}
+	
+	
+ 
 }
--- test/domain/ProjectTest.java
+++ test/domain/ProjectTest.java
@@ -1,11 +1,16 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import domain.Project;
+import domain.Status;
 import static org.junit.Assert.*;
 
 /**
@@ -45,6 +50,16 @@ public class ProjectTest {
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
+    }
+    
+    /**
+     * Test of createTask method, of class Project.
+     */
+    @Test
+    public void testCreateTask() {
+    	Project x = new Project(0, "name", "descr", new GregorianCalendar(), new GregorianCalendar());
+    	x.createTask("bla", 5, 10, 2, new int[]{1,4,5}, Status.AVAILABLE);
+    	
     }
     
 }
