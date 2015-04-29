25c69fcf181abec03d45d9249d94789b301dfb82
Frederic <Frederic@192.168.0.156>
2015-04-29 16:51:13 +0200

    nog wat kleine aanpassingen + een beetje commentaar

--- src/domain/command/ICommand.java
+++ src/domain/command/ICommand.java
@@ -1,5 +1,7 @@
 package domain.command;
 
+import exception.ConflictException;
+
 /**
  * TODO
  * @author Frederic
@@ -8,11 +10,12 @@ package domain.command;
 public interface ICommand {
 	/**
 	 * Executes this command.
+	 * @throws ConflictException TODO ConflictException is een checked exception, dit moet nog gefixed worden.
 	 */
-	void execute();
+	void execute() throws ConflictException;
 	
 	/**
-	 * Reverts the changes caused by the execution of this command.
+	 * Reverts the changes caused by the last execution of this command.
 	 */
 	void revert();
 }
--- src/domain/command/ReservationCommand.java
+++ src/domain/command/ReservationCommand.java
@@ -12,23 +12,34 @@ import exception.ConflictException;
  * 
  * @author Mathias, Frederic, Pieter-Jan
  */
-public class ReservationCommand {
+public class ReservationCommand implements ICommand {
     private final Task task;
     private final Resource resource;
     private Resource.Memento resourceMemento;
     private final Timespan timespan;
     private Reservation reservation;
-    
+    /**
+     * Initializes a new reservation command based on the given time span, resource and task.
+     * 
+     * @param timespan The time span of the reservation
+     * @param resource The resource of the reservation
+     * @param task The task of the reservation
+     */
     public ReservationCommand(Timespan timespan, Resource resource, Task task){
         this.task = task;
         this.resource = resource;
         this.timespan = timespan;
     }
     
-    
+    /**
+     * Creates a new reservation with the timespan, resource and task
+     * belonging to this reservation command.
+     * @throws ConflictException 
+     */
     public void execute() throws ConflictException{
+    	
     	resourceMemento = resource.createMemento();
-        reservation = resource.makeReservation(task, timespan);
+    	reservation = resource.makeReservation(task, timespan);
     }
     
     public void revert(){
