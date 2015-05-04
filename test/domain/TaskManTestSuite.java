c715be49ac3b4369df2ddd8123e528ae6b966063
isostorm <mathiasbenoit@hotmail.com>
2015-05-04 17:20:27 +0200

    init todo's

--- test/domain/TaskManTestSuite.java
+++ test/domain/TaskManTestSuite.java
@@ -1,17 +1,20 @@
 package domain;
 
+import domain.memento.MementoProjectContainerTest;
+import domain.memento.MementoProjectTest;
+import domain.memento.MementoTest;
 import domain.task.TaskTest;
+import domain.time.ClockTest;
+import domain.time.DurationTest;
+import domain.time.TimespanTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-
 import scenariotest.AdvanceSystemTimeScenarioTest;
 import scenariotest.CreateProjectScenarioTest;
 import scenariotest.CreateTaskScenarioTest;
 import scenariotest.ShowProjectScenarioTest;
+import scenariotest.SimulatorScenarioTest;
 import scenariotest.UpdateTaskStatusScenarioTest;
-import domain.time.ClockTest;
-import domain.time.DurationTest;
-import domain.time.TimespanTest;
 
 /**
  * This testsuite, runs all the tests for this application
@@ -27,11 +30,15 @@ import domain.time.TimespanTest;
                     DurationTest.class,
                     ResourceTest.class,
                     ResourceTypeTest.class,
+                    MementoTest.class,
+                    MementoProjectTest.class,
+                    MementoProjectContainerTest.class,
                     AdvanceSystemTimeScenarioTest.class,
                     CreateProjectScenarioTest.class,
                     CreateTaskScenarioTest.class,
                     ShowProjectScenarioTest.class,
-                    UpdateTaskStatusScenarioTest.class})
+                    UpdateTaskStatusScenarioTest.class,
+                    SimulatorScenarioTest.class})
 public class TaskManTestSuite {
     
 }
